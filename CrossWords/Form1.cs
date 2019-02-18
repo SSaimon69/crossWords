c0e883466915adef87da66a23b747ed77a45ac22
semen.sova <semen.sova@yandex.ru>
2019-02-18 14:54:49 +0300

    Текущая версия

---
+++ CrossWords/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrossWords
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Word
+    {
+        string word;
+        string question;
+        bool isVert;
+
+        public Word(string word,string question)
+        {
+            this.word = word;
+            this.question = question;
+        }
+    }
+}
